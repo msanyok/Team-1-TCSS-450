40f360d8861917ee7ac9e692d7387ffc4d81061e
chrisding2333 <chrisding2333@gmail.com>
2021-11-16 14:01:51 -0800

    chatroom and creatchatroom framgments created.

---
+++ app/src/main/java/edu/uw/tcss450/group1project/ui/messages/ChatroomFragment.java
@@ -0,0 +1,25 @@
+package edu.uw.tcss450.group1project.ui.messages;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.group1project.R;
+
+/**
+ * @author Chris Ding
+ * create an instance of this fragment.
+ */
+public class ChatroomFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater theInflater, ViewGroup theContainer,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return theInflater.inflate(R.layout.fragment_chatroom, theContainer, false);
+    }
+}
