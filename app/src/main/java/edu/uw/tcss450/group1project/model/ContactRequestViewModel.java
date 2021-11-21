0c25479c90530116c712fac0ac60357226fdb1d7
Steven Omegna <skomegna@uw.edu>
2021-11-21 13:35:39 -0800

    -Fixed Documentation

--- app/src/main/java/edu/uw/tcss450/group1project/model/ContactRequestViewModel.java
+++ app/src/main/java/edu/uw/tcss450/group1project/model/ContactRequestViewModel.java
@@ -80,7 +80,7 @@ public class ContactRequestViewModel extends AndroidViewModel {
      * @throws NullPointerException if theObserver is null
      */
     public void addRequestObserver(@NonNull final LifecycleOwner theOwner,
-                                          @NonNull final Observer<? super JSONObject> theObserver) {
+                                   @NonNull final Observer<? super JSONObject> theObserver) {
         Objects.requireNonNull(theOwner, "theOwner can not be null");
         Objects.requireNonNull(theObserver, "theObserver can not be null");
         mRequestResponse.observe(theOwner, theObserver);
